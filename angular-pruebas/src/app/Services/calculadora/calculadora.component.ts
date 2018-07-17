b021e5467a808a4f3ecbb089ed9f40c0e0ae4690
Elena Medina <emedinamaza@deloitte.es>
2018-07-17 14:55:48 +0200

    Calculadora

--- angular-pruebas/src/app/Services/calculadora/calculadora.component.ts
+++ angular-pruebas/src/app/Services/calculadora/calculadora.component.ts
@@ -1,23 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
+
 
 @Component({
-  selector: 'app-calculadora',
+  selector: 'calculadora',
   templateUrl: './calculadora.component.html',
   styleUrls: ['./calculadora.component.css']
 })
-export class CalculadoraComponent implements OnInit {
-  var op1:number=0;
-  var op2:number=0;
+export class CalculadoraComponent {
+ public op1:number;
+  public op2:number;
+  resultado;
+  res;
 
   constructor() { }
 
   ngOnInit() {
+
+
   }
+
+  imprimirnumero1( evento){
+  this.op1=evento.key;
   
-2
-3
-function sum(): number{
-    var sum=this.op1+this.op2;
-  return sum ;
-}
+  }
+  imprimirnumero2( evento){
+    this.op2=evento.key;
+   
+    }
+
+  sumar() {
+    this.res = this.op1 + this.op2;
+  }
+
+  restar() {
+    this.res = this.op1 - this.op2;
+  }
+
+  multiplicar() {
+    this.res = this.op1 * this.op2;
+  }
+
+  dividir() {
+    this.res = this.op1 / this.op2;
+  }
+
 }
